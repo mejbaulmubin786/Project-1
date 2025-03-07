ae1903eb72b661b12592600fc01c6a237beb9d4a
Mejbaul Mubin <mejbaul_mubin@yahoo.com>
2025-03-08 00:29:31 +0600

    about section start

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.querySelector(".menu-toggle");
+  const menu = document.querySelector(".menu");
+
+  menuToggle.addEventListener("click", () => {
+    menu.classList.toggle("show");
+    console.log("Menu Clicked!");
+  });
+});
